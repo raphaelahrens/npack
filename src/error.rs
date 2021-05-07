633170e9dc19eb5c35322d4c77c31ad0826e3e2a
Raphael Ahrens <raphaelahrens@googlemail.com>
2021-05-07 16:56:48 +0200

    replaced the call to description() with thiserror
    description seems to be deprecated

--- src/error.rs
+++ src/error.rs
@@ -1,105 +1,51 @@
-use std::error::Error as StdError;
-use std::fmt;
 use std::io;
 use std::path::Path;
-use std::path::StripPrefixError;
 use std::result::Result as StdResult;
 
 use git2;
 use walkdir;
-use yaml_rust::{EmitError, ScanError};
+use thiserror::Error as ThisError;
 
 pub type Result<T> = StdResult<T, Error>;
 
-#[derive(Debug)]
+#[derive(ThisError, Debug)]
 pub enum Error {
-    Io(io::Error),
+    #[error("Io Error {0}")]
+    Io(#[from] io::Error),
+    #[error("Format")]
     Format,
-    Git(String),
+    #[error("Git {0}")]
+    Git(#[from] git2::Error),
+    #[error("")]
     Editor,
+    #[error("Fail to build plugin: {0}")]
     Build(String),
+    #[error("Plugin is not installed")]
     PluginNotInstalled,
+    #[error("NoPlugin")]
     NoPlugin,
+    #[error("SkipLocal")]
     SkipLocal,
+    #[error("{0}")]
     PluginInstalled(String),
+    #[error("")]
     PackFile(String),
-    CopyDir(String),
-    SaveYaml,
-    LoadYaml,
+    #[error("Fail to copy directory: {0}")]
+    CopyDir(#[from] std::path::StripPrefixError),
+    #[error("Fail to copy directory: {0}")]
+    WalkDir(#[from] walkdir::Error),
+    #[error("" )]
+    SaveYaml(#[from] yaml_rust::EmitError),
+    #[error("")]
+    LoadYaml(#[from] yaml_rust::ScanError),
 }
 
 impl Error {
-    pub fn copy_dir(s: &str) -> Error {
-        Error::CopyDir(format!("Fail to copy directory: {}", s))
-    }
-
     pub fn build<T: AsRef<str>>(s: T) -> Error {
-        Error::Build(format!("Fail to build plugin: {}", s.as_ref()))
+        Error::Build(s.as_ref().to_string())
     }
 
     pub fn plugin_installed<T: AsRef<Path>>(s: T) -> Error {
         Error::PluginInstalled(format!("Plugin already installed under {:?}", s.as_ref()))
-    }
-}
-
-impl From<io::Error> for Error {
-    fn from(err: io::Error) -> Error {
-        Error::Io(err)
-    }
-}
-
-impl From<git2::Error> for Error {
-    fn from(err: git2::Error) -> Error {
-        Error::Git(err.description().to_string())
-    }
-}
-
-impl From<walkdir::Error> for Error {
-    fn from(err: walkdir::Error) -> Error {
-        Error::copy_dir(err.description())
-    }
-}
-
-impl From<StripPrefixError> for Error {
-    fn from(err: StripPrefixError) -> Error {
-        Error::copy_dir(err.description())
-    }
-}
-
-impl From<EmitError> for Error {
-    fn from(_: EmitError) -> Error {
-        Error::SaveYaml
-    }
-}
-
-impl From<ScanError> for Error {
-    fn from(_: ScanError) -> Error {
-        Error::LoadYaml
-    }
-}
-
-impl StdError for Error {
-    fn description(&self) -> &str {
-        match *self {
-            Error::Format => "Invalid format",
-            Error::SaveYaml => "Fail to save packfile",
-            Error::LoadYaml => "Fail to load packfile",
-            Error::Editor => "Can not open editor",
-            Error::PluginNotInstalled => "Plugin not installed",
-            Error::NoPlugin => "Can not find such plugin",
-            Error::SkipLocal => "Local plugin. Skipping",
-            Error::Io(ref e) => e.description(),
-            Error::Build(ref s)
-            | Error::Git(ref s)
-            | Error::CopyDir(ref s)
-            | Error::PluginInstalled(ref s)
-            | Error::PackFile(ref s) => s,
-        }
-    }
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.description())
     }
 }
